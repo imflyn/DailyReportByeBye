72bcae564eced08c4992ff1efe3c4e2db626ea82
imflyn <imflyn@163.com>
2016-07-25 21:46:43 +0800

    first commit

---
+++ mail_sender.py
@@ -0,0 +1,31 @@
+from email.mime.text import MIMEText
+import smtplib
+from email.header import Header
+
+
+class MailSender(object):
+    def __init__(self):
+        self.my_name = ''
+        self.from_address = ''
+        self.password = ''
+        self.to_address = ''
+        self.smtp_server = ''
+        self.smtp_server_port = ''
+        self.title = ''
+
+    def send(self, content):
+        msg = MIMEText(content, 'plain', 'utf-8')
+        from_address = self.my_name + '<' + self.from_address + '>'
+        msg['From'] = from_address
+        msg['To'] = self.to_address
+        msg['Subject'] = Header(self.title, 'utf-8')
+
+        try:
+            server = smtplib.SMTP(self.smtp_server, self.smtp_server_port)
+            server.set_debuglevel(1)
+            server.login(self.from_address, self.password)
+            server.sendmail(self.from_address, self.to_address, msg.as_string())
+        except Exception as e:
+            raise Exception(e)
+        finally:
+            server.quit()
---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+import configparser
+
+# 2016_07_25
+from mail_sender import MailSender
+
+date = time.strftime('%Y%m%d', time.localtime(time.time()))
+title = 'Daily_Report_Flyn_%s' % date
+
+configparser = configparser.ConfigParser()
+configparser.read('config.txt')
+
+mail_sender = MailSender()
+mail_sender.to_address = configparser.get("Info", "to_address")
+mail_sender.from_address = configparser.get("Info", "from_address")
+mail_sender.my_name = configparser.get("Info", "my_name")
+mail_sender.password = configparser.get("Info", "password")
+mail_sender.smtp_server = configparser.get("Info", "smtp_server")
+mail_sender.smtp_server_port = configparser.get("Info", "smtp_server_port")
+mail_sender.title = title
+mail_sender.send('this is a test email !')
