cd6b47d7eb3a19554af187dd0e8f237f955540cb
imflyn <imflyn@163.com>
2016-07-27 08:52:05 +0800

    read and format git log (50%)

--- git.py
+++ git.py
@@ -1,20 +1,34 @@
-import os, shutil
+import os, shutil, sys
+
+log_folder_path = os.path.dirname(os.path.realpath(__file__)) + "\git_logs"
+log_file_path = log_folder_path + "\git_log_" + "%s.txt"
 
 
-def save_commit_logs(paths: list, author: str):
+def save_commit_logs(path_list: list, author: str, callback):
     if os.path.exists("git_logs"):
         shutil.rmtree("git_logs")
     os.mkdir("git_logs")
     # for path in paths:
     #     cmd = "cd " + path + " & git fetch "
     #     os.system(cmd)
-    script_path = os.path.dirname(os.path.realpath(__file__))
-    for value, path in enumerate(paths):
+    for value, path in enumerate(path_list):
         cmd = "cd " + path + \
-              "& git log --author=\"" + author + "\" --since=\"24hours\" --all > " + script_path + "\git_logs\git_log_" + str(value + 1) + ".txt"
-        os.system(cmd)
+              "& git log --author=\"" + author + "\" --since=\"48hours\" --all > " + log_file_path % str(value + 1)
+        result = os.popen(cmd).readlines()
+        print(result)
+    if callback is not None:
+        callback()
+
+
+def read_commit_logs():
+    for filename in os.listdir(log_folder_path):
+        file = open(log_folder_path + '\\' + filename)
+        lines = file.readlines()
+        for line in lines:
+            if 'commit' 'Author' 'Date' in line:
+                continue
 
 
 if __name__ == '__main__':
-    paths = []
-    save_commit_logs(paths, 'flyn.yu')
+    paths = [os.path.dirname(os.path.realpath(__file__))]
+    save_commit_logs(paths, 'imflyn', read_commit_logs)
